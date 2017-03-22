1724de07608711a9426227351d63dbb0b61a731f
LiutongZhou <liutong.zhou@columbia.edu>
2017-03-22 15:53:38 -0400

    initialize the visualization function

--- +NYCTaxi/+GenerateDemand/DemandVis.m
+++ +NYCTaxi/+GenerateDemand/DemandVis.m
@@ -1,6 +1,52 @@
-function [ output_args ] = DemandVis( input_args )
+function [ output_args ] = DemandVis( demand,R )
 %DEMANDVIS 此处显示有关此函数的摘要
 %   此处显示详细说明
+demand=Demand.demand{1}(:,:,1);
+
+%% imagesc
+figure
+im=imagesc(demand);
+
+%% Geo show Base Map
+nycshape=shaperead('nycshape\manhattan.shp',...
+    'Selector',{@(BoroName) strcmpi(BoroName,'Manhattan'),'BoroName'},'UseGeoCoords',true) ;
+figure
+usamap(zeros(R.RasterSize),R);
+f1=geoshow(nycshape);
+%% Contour on top of Geoshape base map
+usamap(zeros(R.RasterSize),R);
+contourfm(demand,R)
+cb=contourcbar(gca,'Location','southoutside');
+cb.XLabel.String='haha';
+%contourcmap('jet', 'Colorbar', 'on', ...
+   'Location', 'horizontal', ...
+   'TitleString', 'Contour Intervals in Meters');
+
+%% Surface Plot on top of Contour
+usamap(zeros(R.RasterSize),R);
+f=geoshow(demand,R,'DisplayType','surface','ZData',demand+2500,'CData',demand)
+f.FaceAlpha=0.5;
+view(3)
+tightmap
+daspectm('m',2)
+
+%% Mesh Base Map
+usamap(zeros(R.RasterSize),R);
+f4=geoshow(Demand.demand{1}(:,:,1),R,'DisplayType','mesh')
+%% texture Mesh Map on top of Mesh Base Map
+usamap(zeros(R.RasterSize),R);
+f5=geoshow(demand,R,'DisplayType','texturemap');
+%% 3-D lighted shaded relief of regular data grid on top of texture Mesh Map
+usamap(zeros(R.RasterSize),R);
+f2= meshlsrm(demand,R,[0,90]);
+maptool
+%% add stem bar
+h=stem3m(40.8,-74,1000,'LineWidth',10)
+
+%% Manhattan All Year Round
+ colormap(gray(256)) 
+
+%% Bar
 b=bar3(Demand.demand{1}');
 lift=20*max(max(b(1).ZData));
 for k = 1:length(b)
@@ -52,7 +98,7 @@ caxis([0 threshold])                                    % make color axis scalin
 daspect(dar)                                            % adjust ratio
 set(gca,'ydir','normal'); 
 
-%%
+%% Animation
 
 % %% vis
 % for i=1:size(Demand,3)
