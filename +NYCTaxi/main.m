38ee637d9ddfcd1ad0581a95d1efef1897a3513d
LiutongZhou <liutong.zhou@columbia.edu>
2017-03-25 19:43:25 -0400

    speed optimization

--- +NYCTaxi/main.m
+++ +NYCTaxi/main.m
@@ -1,6 +1,7 @@
+clear;clc;close all;
 import NYCTaxi.*
 %% unzip data downloaded from google cloud
-clear;clc;close all;
+
 file_dir='S:\DataBackup\cleanyellow';
 try
     GenerateDemand.unzipdata(file_dir)
@@ -12,7 +13,7 @@ files=dir(file_dir);
 files=files(~[files.isdir]);
 filenames=fullfile({files.folder}',{files.name}');
 %ds = datastore(filenames,'Delimiter',',','TreatAsMissing', 'NA');
-ds = datastore(filenames{1},'Delimiter',',','TreatAsMissing', 'NA');
+ds = datastore(filenames(1),'Delimiter',',','TreatAsMissing', 'NA','ReadSize','file');
 ds.SelectedVariableNames = {'pickup_datetime', 'dropoff_datetime', 'pickup_longitude',...
     'pickup_latitude','dropoff_longitude','dropoff_latitude','passenger_count','trip_distance','duration'};
 clearvars files filenames;
@@ -20,7 +21,7 @@ clearvars files filenames;
 % latlimit [40.680276,40.882530], lonlimit [-74.036206,-73.909863]  
 % in NYC 0.009 lat ~=1km and 0.0119lon ~= 1km
 [Demand,R]=GenerateDemand.DemandGen(ds,60,[40.680276,40.882530],[-74.036206,-73.909863],0.5);
-save('D:\OneDrive - Columbia University\2017Spring\Research\Data\Data\Demand.mat','Demand','R');
+%save('D:\OneDrive - Columbia University\2017Spring\Research\Data\Data\Demand.mat','Demand','R');
 
 %% save to hdf5
 % x=h5read('D:\Downloads\BJ16_M32x32_T30_InOut.h5','/data');
