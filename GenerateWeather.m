aeb90c4132ce3f19951771f1a4d8363fb3f49825
LiutongZhou <liutong.zhou@columbia.edu>
2017-04-05 12:23:43 -0400

    check weather

--- GenerateWeather.m
+++ GenerateWeather.m
@@ -1,9 +1,25 @@
-file='D:/Downloads/BJ_Meteorology.h5';
-h5disp(file)
+%% Read Meteorology.h5
+
+file='D:\OneDrive - Columbia University\2017Spring\Research\Data\BJ_Meteorology.h5';
+%h5disp(file);
 tb=table();
-tb.date=h5read(file,'/date');
-tb.weather=[h5read(file,'/Weather')]';
-%%
+tb.Date=h5read(file,'/date');
+tb.Weather=[h5read(file,'/Weather')]';
+tb.Temperature=h5read(file,'/Temperature');
+tb.WindSpeed=h5read(file,'/WindSpeed');
+tb(1:5,:)
+%% unzip data
+
 import NYCTaxi.*
-unzipdata('S:\DataBackup\Weather')
-%%
+file_dir='S:\DataBackup\Weather';
+unzipdata(file_dire);
+%% select Central Park Station
+
+files=dir([file_dir,'\*.txt']);
+files=fullfile({files.folder}',{files.name}');
+tbs={};
+for i=1:length(files)
+ds=datastore(files{i},'Delimiter',{',','|'});
+ds.SelectedFormats(:)={'%s'};
+tbs{i}=ds;
+end
